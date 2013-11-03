5e5d67aef2997a0c7bc210f3580f62b2460f8ecc
anurag kumar <anurag3rdsep@gmail.com>
2013-11-03 17:37:54 +0530

    completed backend

--- play/urls.py
+++ play/urls.py
@@ -3,6 +3,7 @@ from play import views
 
 urlpatterns = patterns('',
     url(r'^home', views.home, name='home'),
-    url(r'^about', views.about, name='about')
+    url(r'^about', views.about, name='about'),
+    url(r'^fb', views.fb, name='fb'),
 
 )
--- play/views.py
+++ play/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+import fbconsole
+import re
+import time
+import sys
+from google import search
+from random import choice
+import webbrowser
 
 def home(request):
     return render(request, 'play/home.html')
@@ -8,3 +15,26 @@ def home(request):
 
 def about(request):
     return render(request, 'play/about.html')
+
+def fb(request):
+
+    fbconsole.AUTH_SCOPE = ['user_likes','publish_checkins' ]
+    fbconsole.authenticate()
+    music = fbconsole.fql("SELECT music FROM user  WHERE uid=me()")
+    fbconsole.logout()
+
+    music_list = music[0].values()
+    for name in music_list:	#fetch music names
+        music_name = (choice(name.split(','))).encode('utf-8')
+        
+    for url in search('%s gaana' %music_name, stop=1):
+        if re.search(ur'http://gaana.com', url, re.UNICODE):
+            webbrowser.open_new(url)
+            break
+        else:
+            return HttpResponseRedirect('/play/error') 
+    
+    return render(request, 'play/over.html')
+          
+
+
