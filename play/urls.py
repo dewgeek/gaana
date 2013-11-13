e7a989370a202f0552f76ae7ce9d4afcfe8418f9
anurag kumar <anurag3rdsep@gmail.com>
2013-11-13 11:11:58 +0530

    app complete

--- play/urls.py
+++ play/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from play import views
 
 urlpatterns = patterns('',
-    url(r'^home', views.home, name='home'),
-    url(r'^about', views.about, name='about'),
-    url(r'^fb', views.fb, name='fb'),
+    url(r'^home/', views.home, name='home'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^music/', views.fb, name='fb'),
 
 )
--- play/views.py
+++ play/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, render_to_response
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
 from django.template import RequestContext
 import fbconsole
 import re
@@ -16,23 +17,23 @@ def home(request):
 def about(request):
     return render(request, 'play/about.html')
 
-def fb(request):
+def music(request):
 
     fbconsole.AUTH_SCOPE = ['user_likes','publish_checkins' ]
     fbconsole.authenticate()
     music = fbconsole.fql("SELECT music FROM user  WHERE uid=me()")
-    fbconsole.logout()
+    
 
     music_list = music[0].values()
     for name in music_list:	#fetch music names
         music_name = (choice(name.split(','))).encode('utf-8')
         
-    for url in search('%s gaana' %music_name, stop=1):
+    for url in search('%s gaana' %music_name, stop=1):    #validating if the music are avalialble
         if re.search(ur'http://gaana.com', url, re.UNICODE):
             webbrowser.open_new(url)
             break
         else:
-            return HttpResponseRedirect('/play/error') 
+            return HttpResponseRedirect(reverse(music)) 
     
     return render(request, 'play/over.html')
           
