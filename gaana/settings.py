e7f2f070dbdd5627773d78b5cc5be704c4ba8313
rakesh <mailme.rakeshg@gmail.com>
2014-01-01 00:13:24 +0530

    This is a message

--- gaana/settings.py
+++ gaana/settings.py
@@ -176,11 +176,11 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-"""STATIC_ROOT = 'staticfiles'"""
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'play/static'),
+    os.path.join(BASE_DIR, '../play/static'),
 )
 
 ADMIN_MEDIA_PREFIX = '/static/admin/'
