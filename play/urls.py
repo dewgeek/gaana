273c95a0530d1fd09e9651b8a1f8f81d55c1de4c
rakesh <mailme.rakeshg@gmail.com>
2014-01-01 01:24:10 +0530

    This is a message

--- play/urls.py
+++ play/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     url(r'^home/', views.home, name='home'),
     url(r'^about/', views.about, name='about'),
     url(r'^music/', views.music, name='music'),
-	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+
 )
 
