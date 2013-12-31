85211a1ae22dbe0ea3895f97fbbf57b096aeb1ca
rakesh <mailme.rakeshg@gmail.com>
2014-01-01 00:54:53 +0530

    This is a message

--- play/urls.py
+++ play/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from play import views
-
+from django.conf import settings
 urlpatterns = patterns('',
     url(r'^home/', views.home, name='home'),
     url(r'^about/', views.about, name='about'),
